bcd4f48a462d7408d9d7be0f884f29b10e2a7120
Meli <ms.melcastro@gmail.com>
2026-04-22 16:46:47 -0700

    form building

--- js/script-ems.js
+++ js/script-ems.js
@@ -1,15 +1,27 @@
 // CREATE AN ARRAY OF EMPLOYEES
+"use strict";
 
+let employees = [
+    [10000001, "Melissa Green", 1234, "melissa@email.com", "Marketing"],
+    [10000002, "John Smith", 2222, "john@email.com", "Sales"],
+    [10000003, "Sarah Lee", 3333, "sarah@email.com", "Engineering"],
+    [10000004, "David Cruz", 4444, "david@email.com", "QA"],
+    [10000005, "Jessica Ray", 5555, "jess@email.com", "Executive"]
+];
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
 // IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
-
+if (localStorage.getItem("employees")) {
+    employees = JSON.parse(localStorage.getItem("employees"));
+}
 
 // GET DOM ELEMENTS
-
+const form = document.getElementById("addForm");
+const empTable = document.getElementById("empTable");
+const empCount = document.getElementById("empCount");
 
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
-
+buildGrid();
 
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
