14900e74fc8cc8b11be5412f22ea64a706c20e28
Meli <ms.melcastro@gmail.com>
2026-04-22 11:55:29 -0700

    step 4

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,5 +1,5 @@
 //STEP 1
-let movies = ["13 Going on 30", "The Proposal", "Crazy Rich Asians", "How to Lose a Guy in 10 Days", "27 Dresses"];
+let movies = ["13 Going on 30", "The Proposal", "Crazy Rich Asians", "Clueless", "27 Dresses"];
 console.log(movies[1]);
 
 //STEP 2
@@ -7,7 +7,7 @@ movies = new Array(5);
 movies[0] = "13 Going on 30";
 movies[1] = "The Proposal";
 movies[2] = "Crazy Rich Asians";
-movies[3] = "How to Lose a Guy in 10 Days";
+movies[3] = "Clueless";
 movies[4] = "27 Dresses";
 
 console.log(movies[0]);
@@ -17,13 +17,16 @@ movies = new Array(5);
 movies[0] = "13 Going on 30";
 movies[1] = "The Proposal";
 movies[2] = "Crazy Rich Asians";
-movies[3] = "How to Lose a Guy in 10 Days";
+movies[3] = "Clueless";
 movies[4] = "27 Dresses";
 
 movies.splice(2, 0, "The Wedding Planner");
 console.log(movies.length);
 
 //STEP 4
+movies = ["13 Going on 30", "The Proposal", "Crazy Rich Asians", "Clueless", "27 Dresses"];
+delete movies[0];
+console.log(movies);
 
 //STEP 5
 
