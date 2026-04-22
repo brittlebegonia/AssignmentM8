b410cba185c4766589b7b7f7caa772880069c1b6
Meli <ms.melcastro@gmail.com>
2026-04-22 11:28:16 -0700

    movies

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,6 +1,16 @@
 //STEP 1
+let movies = ["13 Going on 30", "The Proposal", "Crazy Rich Asians", "How to Lose a Guy in 10 Days", "27 Dresses"];
+console.log(movies[1]);
 
 //STEP 2
+movies = new Array(5);
+movies[0] = "13 Going on 30";
+movies[1] = "The Proposal";
+movies[2] = "Crazy Rich Asians";
+movies[3] = "How to Lose a Guy in 10 Days";
+movies[4] = "27 Dresses";
+
+console.log(movies[0]);
 
 //STEP 3
 
